005190ca888760575d897863d45725cbec34a4fc
Graeme Nathan <graemenathan@gmail.com>
2022-09-09 11:03:26 -0700

    Store accessToken in refreshToken

--- jwt/repository.go
+++ jwt/repository.go
@@ -12,21 +12,38 @@ import (
 
 var db *gorm.DB
 
+// RefreshToken represents a refresh token in the databaseh
 type RefreshToken struct {
-	ID       uuid.UUID `gorm:"primary_key;type:uuid;"`
-	FamilyId uuid.UUID `gorm:"index"`
-	Content  string    `gorm:"unique"`
-	Valid    bool      `gorm:"default:true"`
+	// ID is the ID of the refresh token
+	ID uuid.UUID `gorm:"primary_key;type:uuid;"`
+	// FamilyId is the ID of the refresh token family that this refresh token belongs to.
+	// All refresh tokens in the same family are invalidated when one refresh token is invalidated.
+	// Unique by AuthToken.
+	Content string `gorm:"unique"`
+	// AuthToken is the auth token that the refresh token is associated with.
+	// All refresh tokens are either decendants of another refresh token or are
+	// the first refresh token in a family directly related to the auth token that
+	// it is associated with.
+	AuthToken string `gorm:"index:idx_auth_token"`
+	// Valid is whether or not the refresh token is valid.
+	// When a refresh token is invalidated, all refresh tokens in the same family are invalidated.
+	Valid bool `gorm:"default:true"`
 }
 
-func InsertRefreshTokenNewFamily(content string) (id uuid.UUID, err error) {
+// InsertRefreshTokenNewFamily inserts a new refresh token into the database
+// with a new family ID.
+// This is used when a new refresh token is generated from a new sign in.
+//
+// param content: The content of the refresh token
+// param authToken: The auth token that the refresh token is associated with
+// returns: The ID of the new refresh token
+func InsertRefreshTokenNewFamily(content string, authToken string) (id uuid.UUID, err error) {
 	id = uuid.New()
-	familyId := uuid.New()
 
 	refreshToken := RefreshToken{
-		ID:       id,
-		FamilyId: familyId,
-		Content:  content,
+		ID:        id,
+		Content:   content,
+		AuthToken: authToken,
 	}
 
 	db.Create(&refreshToken)
@@ -34,18 +51,21 @@ func InsertRefreshTokenNewFamily(content string) (id uuid.UUID, err error) {
 	return id, nil
 }
 
-func InsertRefreshTokenExistingFamily(content string, familyId string) (id uuid.UUID, err error) {
+// InsertRefreshTokenExistingFamily inserts a new refresh token into the database
+// with the same family ID as the refresh token with the given ID.
+// This is used when a refresh token is used to generate a new refresh token.
+func InsertRefreshTokenExistingFamily(content string, authToken string) (id uuid.UUID, err error) {
 	var existing RefreshToken
-	db.First(&existing, "ID = ?", familyId)
+	db.First(&existing, "AuthToken = ?", authToken)
 
 	if existing.ID == uuid.Nil {
 		return uuid.Nil, errors.New("Refresh token family not found")
 	}
 
 	refreshToken := RefreshToken{
-		ID:       uuid.New(),
-		FamilyId: existing.FamilyId,
-		Content:  content,
+		ID:        uuid.New(),
+		AuthToken: existing.AuthToken,
+		Content:   content,
 	}
 
 	db.Create(&refreshToken)
@@ -53,12 +73,24 @@ func InsertRefreshTokenExistingFamily(content string, familyId string) (id uuid.
 	return refreshToken.ID, nil
 }
 
+// GetRefreshTokenByContents returns a refresh token found by its content.
+// Typically used to check if a refresh token is valid.
 func GetRefreshTokenByContents(contents string) (RefreshToken, error) {
-	return RefreshToken{}, nil
+	var refreshToken RefreshToken
+
+	db.First(&refreshToken, "Content = ?", contents)
+
+	if refreshToken == (RefreshToken{}) {
+		return refreshToken, errors.New("Refresh token not found")
+	}
+
+	return refreshToken, nil
 }
 
-// invalidateRefreshTokenTree invalidates all refresh tokens that are children of the given refresh token
-func InvalidateRefreshTokenTree(refreshToken RefreshToken) error {
+// invalidateRefreshTokenTree invalidates all refresh tokens in the family tree
+func InvalidateRefreshTokenTree(authToken string) error {
+	db.Model(&RefreshToken{}).Where("AuthToken = ?", authToken).Update("Valid", false)
+
 	return nil
 }
 
@@ -94,8 +126,8 @@ func seed() {
 	db.First(&seeded, "ID = ?", seededId)
 
 	if seeded == (RefreshToken{}) {
-		fmt.Println("Seeding database")
-		_, err := InsertRefreshTokenNewFamily("test")
+		fmt.Println("Seeding database `jwt-service.db` with refresh token")
+		_, err := InsertRefreshTokenNewFamily("test", "auth-token")
 		if err != nil {
 			panic(err)
 		}
